11fd63d8b7e80f4517b7fc84f24cffc26fbc40a4
Rafael Fernandes <rafatexfr@gmail.com>
2019-12-08 22:47:20 +0100

    added prettier, travis and jest configuration files

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest"
+  preset: '@vue/cli-plugin-unit-jest',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{js,ts,vue}',
+    '!<rootDir>/src/main.js',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text-summary'],
+  testMatch: ['<rootDir>/src/**/?(*.)+(spec|test).[jt]s?(x)'],
 };
