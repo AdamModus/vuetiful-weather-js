31bb4544a5ea10dc03b4d723e41db8c94a167bb3
Rafael Fernandes <rafatexfr@gmail.com>
2019-12-10 08:38:43 +0100

    made unit tests not fail and created first example of default vue unit test

--- src/components/CurrentWeather/CurrentWeather.spec.js
+++ src/components/CurrentWeather/CurrentWeather.spec.js
@@ -1,12 +1,28 @@
-import { shallowMount } from '@vue/test-utils';
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import Vuex from 'vuex';
 import CurrentWeather from './CurrentWeather.vue';
 
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
 describe('CurrentWeather.vue', () => {
+  let store;
+  let state = {
+    forecast: {
+      currentWeatherForecast: undefined,
+    },
+  };
+  let mutations = {};
+  let actions = {};
+  let getters = {};
+  let modules = {};
+
+  beforeEach(() => {
+    store = new Vuex.Store({ state, mutations, actions, getters, modules });
+  });
+
   it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(CurrentWeather, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+    const wrapper = shallowMount(CurrentWeather, { store, localVue });
+    expect(wrapper).toBeTruthy();
   });
 });
