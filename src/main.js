e8ed9178586e32978fdbec59ff6b3127fc5fc251
Rafael Fernandes <rafatexfr@gmail.com>
2019-12-24 16:27:40 +0000

    now using vue cli pwa plugin

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import vuetify from './plugins/vuetify';
 import router from './router';
 import store from './store';
 import './../node_modules/weather-icons/css/weather-icons.min.css';
+import './registerServiceWorker'
 
 Vue.config.productionTip = false;
 
