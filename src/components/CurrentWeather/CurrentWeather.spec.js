64d30c393da0710b5fb9d73dae71a1983fb668a2
Rafael Fernandes <rafatexfr@gmail.com>
2020-01-17 15:55:04 +0100

    added unit tests for current weather component

--- src/components/CurrentWeather/CurrentWeather.spec.js
+++ src/components/CurrentWeather/CurrentWeather.spec.js
@@ -1,28 +1,108 @@
+import services from '@/services/index';
 import { createLocalVue, shallowMount } from '@vue/test-utils';
 import Vuex from 'vuex';
 import CurrentWeather from './CurrentWeather.vue';
+jest.mock('@/services/index');
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
 
 describe('CurrentWeather.vue', () => {
   let store;
-  let state = {
-    forecast: {
-      currentWeatherForecast: undefined,
-    },
+  let state;
+  let mutations;
+  let actions;
+  let getters;
+  let modules;
+
+  const mockedServiceReturns = {
+    mapOWMCodeToIconClass: 'mockedIconClass',
+    getWindCardinalDirection: 'mockCardinalDirection',
+    mpsTokmph: 'mocked MpS to KMpH',
+    convertTemperature: 'mcokedConvertedTemperature',
+    getWindRotationCSS: 'mockedWindRotationCSS',
   };
-  let mutations = {};
-  let actions = {};
-  let getters = {};
-  let modules = {};
 
   beforeEach(() => {
+    services.mapOWMCodeToIconClass.mockImplementation(
+      () => mockedServiceReturns.mapOWMCodeToIconClass
+    );
+    services.getWindCardinalDirection.mockImplementation(
+      () => mockedServiceReturns.getWindCardinalDirection
+    );
+    services.mpsTokmph.mockImplementation(() => mockedServiceReturns.mpsTokmph);
+    services.convertTemperature.mockImplementation(
+      () => mockedServiceReturns.convertTemperature
+    );
+    services.getWindRotationCSS.mockImplementation(
+      () => mockedServiceReturns.getWindRotationCSS
+    );
+
+    mutations = {};
+    actions = {};
+    getters = {};
+    modules = {};
+    state = {
+      temperatureUnit: {
+        unit: 'Celsius',
+      },
+      forecast: {
+        currentWeatherForecast: {
+          description: 'scattered clouds',
+          iconId: 802,
+          temperature: 282.92,
+          feelsTemperature: 277.7,
+          minTemperature: 282.04,
+          maxTemperature: 283.71,
+          humidity: 87,
+          pressure: 1012,
+          windSpeed: 6.7,
+          windDirection: 220,
+          cloudiness: 40,
+        },
+      },
+      location: {
+        city: 'mocked city',
+      },
+    };
     store = new Vuex.Store({ state, mutations, actions, getters, modules });
   });
 
-  it('renders props.msg when passed', () => {
-    const wrapper = shallowMount(CurrentWeather, { store, localVue });
-    expect(wrapper).toBeTruthy();
+  it('Does not render content when currentWeatherForecast in Vuex is not set', () => {
+    const emptyState = {
+      forecast: {
+        currentWeatherForecast: undefined,
+      },
+    };
+    const store = new Vuex.Store({
+      state: emptyState,
+      mutations,
+      actions,
+      getters,
+      modules,
+    });
+    const component = shallowMount(CurrentWeather, { store, localVue });
+    expect(component.find('v-row').exists()).toBeFalsy();
+  });
+
+  it('Computes properties properly using imported services', () => {
+    const component = shallowMount(CurrentWeather, { store, localVue });
+
+    expect(component.vm.weatherIconId).toEqual(
+      mockedServiceReturns.mapOWMCodeToIconClass
+    );
+    expect(component.vm.cardinalDirection).toEqual(
+      mockedServiceReturns.getWindCardinalDirection
+    );
+    expect(component.vm.windSpeedKmph).toEqual(mockedServiceReturns.mpsTokmph);
+    expect(component.vm.temperature).toEqual(
+      mockedServiceReturns.convertTemperature
+    );
+    expect(component.vm.feelsTemperature).toEqual(
+      mockedServiceReturns.convertTemperature
+    );
+    expect(component.vm.windRotationCSS).toEqual(
+      mockedServiceReturns.getWindRotationCSS
+    );
   });
 });
