3943cf30b5d7680283095f46b6460cf2f63fef1d
Rafael Fernandes <rafatexfr@gmail.com>
2020-02-13 08:29:57 +0100

    adjusted settings for editors

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,10 @@ module.exports = {
   },
   overrides: [
     {
-      files: ['**/__tests__/*.{j,t}s?(x)', '**/src/**/*.spec.{j,t}s?(x)'],
+      files: [
+        '**/src/**/*.spec.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
       env: {
         jest: true,
       },
